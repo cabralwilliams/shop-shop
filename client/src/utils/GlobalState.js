d72202d96a09e8bab93db54d080c1e5e3f1ec9c4
Cabral Williams <cabral.williams@gmail.com>
2022-01-18 11:23:29 -0600

    implemented the global store feature

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from "./reducers";
+
+const StoreContext = createContext();
+//Every Context object comes with components Provider and Consumer
+const { Provider } = StoreContext;
+//The Provider component makes state data available to all other components
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: ''
+    });
+    //Confirm that the above worked
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+}
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+}
+
+export { StoreProvider, useStoreContext };
